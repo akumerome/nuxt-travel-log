c4b29070bc2f5fb63a921022aad213e2d092769c
Alvaro KR <akumenius@gmail.com>
2025-07-06 00:23:42 +0200

    typesafe env with zod

---
+++ components/app/color-mode-button.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const colorMode = useColorMode()
+
+const isDark = computed({
+    get() {
+        return colorMode.value === 'dark'
+    },
+    set(value) {
+        colorMode.preference = value ? 'dark' : 'light'
+    }
+})
+</script>
+
+<template>
+    <UButton :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'" color="neutral" variant="outline"
+        @click="isDark = !isDark" />
+</template>
