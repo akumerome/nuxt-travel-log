a5f45c4e2f5910e59c30da7cb0a6db471672a4d3
Alvaro Kumenius <131394063+akumerome@users.noreply.github.com>
2025-07-05 23:34:49 +0200

    add landing page (#49)

---
+++ components/app/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="bg-default border-b border-default h-[4rem] sticky top-0 z-50 w-full">
+        <div class="w-full max-w-[90rem] mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between gap-3 h-full">
+            <div class="lg:flex-1 flex items-center gap-1.5">
+                <NuxtLink to="/">Travel Log</NuxtLink>
+            </div>
+            <div class="hidden lg:flex">
+
+            </div>
+            <div class="flex items-center justify-end lg:flex-1 gap-1.5">
+                <UButton class="border border-inverted" color="neutral" variant="outline">Sign in</UButton>
+                <ColorModeButton></ColorModeButton>
+            </div>
+        </div>
+    </header>
+</template>
