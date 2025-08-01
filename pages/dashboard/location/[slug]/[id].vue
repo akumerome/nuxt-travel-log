92cc31e1642af1c6b76869248821aca82abb369b
Alvaro Kumenius <131394063+akumerome@users.noreply.github.com>
2025-08-02 01:55:02 +0200

    show location logs details (#73)

--- pages/dashboard/location/[slug]/[id].vue
+++ pages/dashboard/location/[slug]/[id].vue
@@ -1,3 +1,36 @@
+<script lang="ts" setup>
+const locationsStore = useLocationsStore();
+const { currentLocationLog: data, currentLocationLogStatus: status, currentLocationLogError: error } = storeToRefs(locationsStore);
+
+onMounted(() => {
+    locationsStore.refreshCurrentLocationLog();
+});
+</script>
+
 <template>
-<h2>This is the location log page</h2>
+<div>
+    <div v-if="status === 'pending'">
+        <AppLoader />
+    </div>
+    <div v-if="status !== 'pending' && error">
+        <UAlert v-if="error" color="error" icon="i-tabler-circle-x" :title="error.statusMessage" />
+    </div>
+    <div v-if="status !== 'pending' && data?.data.location_log">
+        <div class="mb-4">
+            <div class="flex gap-3">
+                <h1 class="text-2xl font-bold">{{ data.data.location_log.name }}</h1>
+                <DeleteLocationModal></DeleteLocationModal>
+            </div>
+            <p class="text-sm text-dimmed">{{ data.data.location_log.description }}</p>
+            <p class="text-sm text-dimmed">
+                <span v-if="data.data.location_log.started_at !== data.data.location_log.ended_at">
+                    {{ formatTimestamp(data.data.location_log.started_at) }} - {{
+                        formatTimestamp(data.data.location_log.ended_at)
+                    }}
+                </span>
+                <span v-else>{{ formatTimestamp(data.data.location_log.started_at) }}</span>
+            </p>
+        </div>
+    </div>
+</div>
 </template>
--- server/api/locations/[slug]/[id].get.ts
+++ server/api/locations/[slug]/[id].get.ts
@@ -1,4 +1,4 @@
-import { Location } from "~/server/models/Location";
+import { LocationLog } from "~/server/models/LocationLog";
 
 export default defineEventHandler(async (event) => {
     try {
@@ -7,21 +7,21 @@ export default defineEventHandler(async (event) => {
         // This will throw a 401 error if the request doesn't come from a valid user session
         const { user } = await requireUserSession(event);
 
-        const slug = getRouterParam(event, "slug") as string;
-        const location = await Location.deleteOne({ slug, user: user._id });
+        const _id = getRouterParam(event, "id") as string;
+        const locationLog = await LocationLog.findOne({ _id, user: user._id }).lean();
 
-        if (!location.deletedCount) {
+        if (!locationLog) {
             return sendError(event, createError({
                 statusCode: 404,
-                statusMessage: "Location not found.",
+                statusMessage: "Location log not found.",
             }));
         }
 
         return {
-            statusCode: 204,
-            statusMessage: "Location deleted successfully.",
+            statusCode: 200,
+            statusMessage: "Location log retrieved successfully.",
             data: {
-                location
+                location_log: locationLog
             },
         };
 
@@ -29,7 +29,7 @@ export default defineEventHandler(async (event) => {
         console.error(error);
         return sendError(event, createError({
             statusCode: 500,
-            statusMessage: "An error occurred while deleting the location.",
+            statusMessage: "An error occurred while retrieving the location log.",
         }));
     }
 });
