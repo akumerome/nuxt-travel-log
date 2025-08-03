a238173865094895fbddf831a466466647f55c6c
Alvaro KR <akumenius@gmail.com>
2025-08-03 23:52:17 +0200

    setup minio container

--- lib/env.ts
+++ lib/env.ts
@@ -5,6 +5,11 @@ const EnvSchema = z.object({
   MONGO_URI: z.string(),
   MONGO_DATABASE: z.string(),
   NUXT_SESSION_PASSWORD: z.string(),
+  S3_ENDPOINT: z.string(),
+  S3_ACCESS_KEY: z.string(),
+  S3_SECRET_KEY: z.string(),
+  S3_REGION: z.string(),
+  S3_BUCKET: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
